b9a826ee8a75f81a4ce820d8cf211e503b6028f4
GraceHanJiarui <102732338+GraceHanJiarui@users.noreply.github.com>
2022-07-02 22:05:09 +0800

    Add files via upload

---
+++ store/index.js
@@ -0,0 +1,24 @@
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);//vue的插件机制
+
+//Vuex.Store 构造器选项
+const store = new Vuex.Store({
+	actions:{
+		addTokenNumber(context,value){
+			context.commit('ADDTOKENNUMBER',value)
+		}
+	},
+	mutations:{
+		ADDTOKENNUMBER(state,value){
+			state.tokenNumber+=value
+		}
+	},
+	state:{//存放状态
+		tokenNumber:0,
+		
+	}
+})
+export default store
