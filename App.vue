63e6aa10791179aad863cf6ac4e7a78ec453531e
GraceHanJiarui <102732338+GraceHanJiarui@users.noreply.github.com>
2022-07-02 23:52:52 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	.{
+		font-family: "PingFang SC","Hiragino Sans GB","STHeiti","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
+	}
+	/*每个页面公共css */
+</style>
---
+++ main.js
@@ -0,0 +1,30 @@
+import App from './App'
+
+// #ifndef VUE3
+import Vue from 'vue'
+import store from '@/store/index.js'
+
+Vue.config.productionTip = false
+const sysHeight=uni.getSystemInfoSync().screenHeight
+const sysWidth=uni.getSystemInfoSync().screenWidth
+Vue.prototype.hpx = sysHeight/100
+Vue.prototype.wpx = sysWidth/750
+Vue.prototype.$store = store
+
+App.mpType = 'app'
+const app = new Vue({
+	store,
+    ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
